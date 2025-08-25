f662d117c688cfccbac817383b60fc23fb415957
Adrian Popirlan <theravengod@gmail.com>
2025-08-25 21:10:35 +0300

    Rework some logic around selected task

--- todo-lib/src/lib.rs
+++ todo-lib/src/lib.rs
@@ -4,53 +4,76 @@ use crate::task::PrettyPrint;
 use colored::Colorize;
 use console::Term;
 use std::io::Write;
+use uuid::Uuid;
 use task::Task;
 
 pub fn run_mode(args: &Vec<String>) {
     // Some inits
     let term = Term::stdout();
     let mut task_store: Vec<Task> = Vec::new();
+    let mut active_task_id: Option<Uuid> = None;
 
     // Logic
     if args.iter().len() > 0 {
-        show_menu(&term, &mut task_store)
+        show_menu(&term, &mut task_store, &mut active_task_id)
     } else {
         println!("With args")
     }
 }
 
-fn show_menu(term: &Term, store: &mut Vec<Task>) {
+fn show_menu(term: &Term, store: &mut Vec<Task>, active_task_id: &mut Option<Uuid>) {
     println!("..:: {} ::..\n", "To Do Manager".bright_white());
-    show_actions();
+    show_actions(active_task_id.is_some());
 
     let mut should_exit = false;
     while !should_exit {
         print!("Choose your action: ");
         std::io::stdout().flush().unwrap(); // Ensure the prompt is displayed
         let action = term.read_char();
-        match (action) {
+        match action {
             Ok(ch) => {
-                match (ch) {
-                    '1' => create_new_task(term, store),
-                    '2' => search_note_by_title(term, store),
-                    '3' => show_all_tasks(term, store),
-                    'h' | 'H' => show_actions(),
-                    'q' | 'Q' => should_exit = true, // TODO: handle error
-                    _ => println!("Invalid option")
-                };
+                if active_task_id.is_none() {
+                    match ch {
+                        '1' => create_new_task(term, store),
+                        '2' => search_note_by_title(term, store),
+                        '3' => show_all_tasks(term, store),
+                        '4' => println!("Select a task"),
+                        'h' | 'H' => show_actions(active_task_id.is_some()),
+                        'q' | 'Q' => should_exit = true, // TODO: handle error
+                        _ => println!("Invalid option")
+                    };
+                } else {
+                    match ch {
+                        '1' => println!("Show task"),
+                        '2' => println!("Edit task title"),
+                        '3' => println!("Edit task description"),
+                        '4' => println!("Delete task"),
+                        '5' => println!("Clear task"),
+                        'h' | 'H' => show_actions(active_task_id.is_some()),
+                        'q' | 'Q' => should_exit = true, // TODO: handle error
+                        _ => println!("Invalid option")
+                    };
+                }
             }
             Err(_) => {}
         }
     }
 }
 
-fn show_actions() {
+fn show_actions(has_active_task: bool) {
     println!("Possible actions:");
-    println!("{}) Create a task", "1".yellow());
-    println!("{}) Search for a task by title", "2".yellow());
-    println!("{}) Show all tasks titles", "3".yellow());
-    println!("{}) Edit a task", "4".yellow());
-    println!("{}) Delete a task", "5".yellow());
+    if !has_active_task {
+        println!("{}) Create a task", "1".yellow());
+        println!("{}) Search for a task by title", "2".yellow());
+        println!("{}) Show all tasks titles", "3".yellow());
+        println!("{}) Select a task by number", "4".yellow());
+    } else {
+        println!("{}) Show all task info", "1".yellow());
+        println!("{}) Edit the tile", "2".yellow());
+        println!("{}) Edit the description", "3".yellow());
+        println!("{}) Delete the current task", "4".yellow());
+        println!("{}) Clear the selected current task", "5".yellow());
+    }
     println!("{}) Show possible actions", "H".cyan());
     println!("{}) Quit", "Q".red());
 }
